361de1bd0373d3dcbd53c7e1355909c7435d3f7c
Raphael KRIEF <rkrief@e2r6p2.42.fr>
2017-12-11 13:55:06 +0100

    add the modification for ompilation

--- ft_putflags.c
+++ ft_putflags.c
@@ -6,38 +6,34 @@
 /*   By: rkrief <rkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:30:46 by rkrief            #+#    #+#             */
-/*   Updated: 2017/12/11 11:56:10 by rkrief           ###   ########.fr       */
+/*   Updated: 2017/12/11 13:52:13 by rkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putflags(t_case block)
+void	ft_putflags(t_case *block)
 {
 	int	i;
-	int	open;
+	char *str;
 
-	open = 0;
 	i = 0;
 	while ((block->content)[i])
 	{
-		if (ft_isdigit((block->content)[i]) && open == 1)
-			flag->width = 0;
 		if ((block->content)[i] == '+')
-			flag->plus = 1;	
+			block->flag.plus = 1;	
 		if ((block->content)[i] == '-')
-			flag->less = 1;	
+			block->flag.less = 1;	
 		if ((block->content)[i] == '#')
-			flag->sharp = 1;	
+			block->flag.sharp = 1;	
 		if ((block->content)[i] == '0')
-			flag->zero = 1;	
+			block->flag.zero = 1;	
 		if ((block->content)[i] == ' ')
-			flag->space = 1;
-		while (ft_isdigit((block->content)[i]) == 1)
-		{
-			flag->width = (flag->width * 10) + ft_atoi((block->content)[i++]);
-			open = 1;
-		}
+			block->flag.space = 1;
+		if (ft_isdigit((block->content)[i]) == 1)
+			block->flag.width = ft_atoi(1);
+			str = ft_strndup((block->content), i);
+			printf("WATCHOUT = %s\n", str);
 		i++;
 	}	
 }
